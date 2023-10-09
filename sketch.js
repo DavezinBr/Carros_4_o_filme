99b0ad2e41172f0f7860870d0e0a80e127f45f89
DavezinBr <121894689+DavezinBr@users.noreply.github.com>
2023-10-09 15:09:10 -0300

    Update sketch.js
    ISTO É O FILME COMPLETO 100% SEM VIRUS MESMO SERIO NADA PIRATEADO BAIXA NÃO TEM VIRUS

--- sketch.js
+++ sketch.js
@@ -9,16 +9,16 @@ var cars = [];
 var blastImg;
 
 function preload() {
-  backgroundImage = loadImage("./assets/planodefundo.png");
-  car1_img = loadImage("../assets/car1.png");
-  car2_img = loadImage("../assets/car2.png");
-  track = loadImage("../assets/pista.png");
-  fuelImage = loadImage("./assets/fuel.png");
-  powerCoinImage = loadImage("./assets/goldCoin.png");
-  obstacle1Image = loadImage("./assets/obstacle1.png");
-  obstacle2Image = loadImage("./assets/obstacle2.png");
-  lifeImage = loadImage("./assets/life.png");
-  blastImg = loadImage("./assets/blast.png")
+  backgroundImage = loadImage("assets/planodefundo.png");
+  car1_img = loadImage("assets/car1.png");
+  car2_img = loadImage("assets/car2.png");
+  track = loadImage("assets/pista.png");
+  fuelImage = loadImage("assets/fuel.png");
+  powerCoinImage = loadImage("assets/goldCoin.png");
+  obstacle1Image = loadImage("assets/obstacle1.png");
+  obstacle2Image = loadImage("assets/obstacle2.png");
+  lifeImage = loadImage("assets/life.png");
+  blastImg = loadImage("assets/blast.png")
 }
 
 function setup() {
